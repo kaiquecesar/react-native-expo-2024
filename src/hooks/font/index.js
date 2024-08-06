b9e10929399d837d088d827eaa3849685ebccf41
Kaique Cesar da Silva <159555005+kaiquecesar@users.noreply.github.com>
2024-08-06 19:42:16 +0000

    [feat] Expo-font with Context

--- src/hooks/font/index.js
+++ src/hooks/font/index.js
@@ -1,9 +1,29 @@
+import { useFonts } from "expo-font";
 import { createContext, useContext } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
 
 const FontContext = createContext({});
 
 export function FontProvider({ children }) {
-  return <FontContext.Provider value={{}}>{children}</FontContext.Provider>;
+
+  const [loaded, error] = useFonts ({
+    regular: require("../../assets/fonts/Rubik-Regular.ttf"),
+    bold: require("../../assets/fonts/Rubik-Bold.ttf"),
+    italic: require("../../assets/fonts/Rubik-Italic.ttf"), 
+    light: require("../../assets/fonts/Rubik-Light.ttf"),
+    medium: require("../../assets/fonts/Rubik-Medium.ttf"),
+    semibold: require("../../assets/fonts/Rubik-SemiBold.ttf"),
+    black: require("../../assets/fonts/Rubik-Black.ttf"),
+  });
+
+  if (!loaded && !error) {
+    <View style={{flex: 1, justifyContent: "center", alignItems:"center"}}>
+      <Text style={{fontSize: 28, marginTop:15}}>Carregando as fontes</Text>
+      <ActivityIndicator size="large" color="#0000ff"/>;
+    </View>
+  } 
+
+  return <FontContext.Provider value={{loaded}}>{children}</FontContext.Provider>;
 }
 
 export function useFont() {
