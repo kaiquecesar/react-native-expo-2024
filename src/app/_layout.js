744d2affc3d233ffaff43b9ca03c54f4b48b4e0b
Kaique Cesar da Silva <159555005+kaiquecesar@users.noreply.github.com>
2024-08-14 14:11:11 +0000

    [feat] Auth Context

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,10 +1,29 @@
-import { Slot, Stack } from "expo-router";
+import { Stack, useSegments, router } from "expo-router";
 import { AppProvider } from "../hooks";
+import { useAuth } from "../hooks/Auth";
+import { useEffect } from "react";
 
 export default function Layout() {
+    const { user } = useAuth();
+    const segments = useSegments();
+
+    useEffect(() => {
+        const inAuthGroup = segments[0] ==="(protected)";
+
+        if (!user?.autenticated && inAuthGroup) {
+            
+            router.replace("/");
+        } else {
+            router.replace("/(protected)");
+        }
+    }, [user]);
+
     return (
     <AppProvider>
-        <Stack />
+        <Stack>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(protected)" />
+        </Stack>
     </AppProvider>
     );
 }
