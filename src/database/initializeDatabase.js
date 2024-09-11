7882d5cb5577e72e5733782cf8361bc7b354b79c
Kaique Cesar da Silva <159555005+kaiquecesar@users.noreply.github.com>
2024-09-11 12:57:25 +0000

    [feat] Create payments Datatable

--- src/database/initializeDatabase.js
+++ src/database/initializeDatabase.js
@@ -1,17 +1,34 @@
 export async function initializeDatabase(database) {
     try {
         await database.execAsync(`
+            DROP TABLE IF EXISTS payments;
+            
             DROP TABLE IF EXISTS users;
 
             CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
             nome TEXT,
+            curso TEXT,
             email TEXT NOT NULL UNIQUE,
             senha TEXT NOT NULL DEFAULT 'A123456a!',
             role TEXT NOT NULL DEFAULT 'USER', 
             created_at DATE DEFAULT CURRENT_TIMESTAMP, 
             updated_at DATE
             );
+
+            CREATE TABLE IF NOT EXISTS payments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            user_cadastro TEXT NOT NULL,
+            valor_pago REAL NOT NULL,
+            data_pagamento DATE NOT NULL,
+            observacao TEXT,
+            created_at DATE DEFAULT CURRENT_TIMESTAMP, 
+            updated_at DATE,
+            FOREIGN KEY(user_id) REFERENCES users(id),
+            FOREIGN KEY(user_cadastro) REFERENCES users(id)
+            );
+
 
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@gmail.com', 'A123456a!', 'SUPER');
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Admin', 'admin@gmail.com', 'A123456a!', 'ADMIN');
