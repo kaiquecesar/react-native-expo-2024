d6f393ef05cbb138338bbf245f68bfd85cd4839e
Kaique Cesar da Silva <159555005+kaiquecesar@users.noreply.github.com>
2024-10-02 14:11:31 +0000

    [feat] Validando Pagamentos

--- src/app/(protected)/payment.js
+++ src/app/(protected)/payment.js
@@ -1,9 +1,27 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Picker } from "@react-native-picker/picker";
 import { router } from "expo-router";
-import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import { z } from "zod";
+import { useAuth } from "../../hooks/Auth/index";
+
+const paymentSchema = z.object({
+  valor_pago: z.number().gt(0),
+  user_id: z.number().int().positive(),
+  user_cadastro: z.number().int().positive(),
+  data_pagamento: z.date(),
+  observacao: z.string(),
+});
 
 export default function Payment() {
   const [valor, setValor] = useState("0,00");
@@ -53,68 +71,129 @@ export default function Payment() {
   const [data, setData] = useState(new Date());
   const [viewCalendar, setViewCalendar] = useState(false);
   const [observacao, setObservacao] = useState("");
+  const valueRef = useRef();
+  const {user} = useAuth();
 
   const handleCalendar = (event, selectedDate) => {
     setViewCalendar(false);
     setData(selectedDate);
   };
 
+  useEffect(() => {
+    valueRef?.current?.focus();
+  }, []);
+
+  const handleChangeValor = (value) => {
+    try {
+      let valorLimpo = value.replace(",", "").replace(".", "");
+      let valorConvertido = Number(valorLimpo) / 100;
+      if (valorConvertido === 0 || isNaN(valorConvertido)) {
+        setValor("0,00");
+        return;
+      }
+      let valorPtBR = Intl.NumberFormat("pt-BR", {
+        style: "decimal",
+        minimumFractionDigits: 2,
+      }).format(valorConvertido);
+      setValor(valorPtBR);
+    } catch (error) {
+      setValor("0,00");
+    }
+  };
+
+  const convertValue = (value) => {
+    try {
+      let valorLimpo = value.replace(",", "").replace(".", "");
+      let valorConvertido = Number(valorLimpo) / 100;
+      if (valorConvertido === 0 || isNaN(valorConvertido)) {
+        return 0;
+      }
+      return valorConvertido
+    } catch (error) {
+      return valorConvertido
+    }
+  };
+
+  const handleSubmit = async () => {
+    console.log(user.user);
+    const payment = {
+      user_id: id,
+      user_cadastro: Number(user.id),
+      valor_pago: convertValue(valor),
+      data_pagamento: data,
+      observacao,
+    };
+
+    try {
+      const result = await paymentSchema.parseAsync(payment);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View style={styles.content}>
-      <Text style={styles.title}>Inserir Pagamentos</Text>
-      <View style={styles.inputView}>
-        <Ionicons name="wallet-outline" size={24} color="black" />
-        <TextInput
-          placeholder="Valor"
-          keyboardType="decimal-pad"
-          style={styles.inputValor}
-          value={valor}
-          onChangeText={setValor}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <Picker
-          selectedValue={id}
-          onValueChange={(itemVlaue, index) => {
-            setId(itemVlaue);
-          }}
-          style={{ width: "100%" }}
-        >
-          {sugestoes?.map((item) => {
-            return (
-              <Picker.Item key={item.id} label={item.nome} value={item.id} />
-            );
-          })}
-        </Picker>
-      </View>
-      <View style={styles.inputView}>
-        <Text onPress={() => setViewCalendar(true)} style={styles.inputData}>
-          {data.toLocaleDateString().split("T")[0]}
-        </Text>
-        {viewCalendar && (
-          <DateTimePicker
-            value={data}
-            onChange={handleCalendar}
-            mode="date"
-            testID="dateTimePicker"
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.content}>
+        <Text style={styles.title}>Inserir Pagamentos</Text>
+        <View style={styles.inputView}>
+          <Ionicons name="wallet-outline" size={24} color="black" />
+          <TextInput
+            placeholder="Valor"
+            keyboardType="decimal-pad"
+            style={styles.inputValor}
+            value={valor}
+            onChangeText={(newValue) => handleChangeValor(newValue)}
+            ref={valueRef}
           />
-        )}
+        </View>
+        <View style={styles.inputView}>
+          <Picker
+            selectedValue={id}
+            onValueChange={(itemVlaue, index) => {
+              setId(itemVlaue);
+            }}
+            style={{ width: "100%" }}
+          >
+            {sugestoes?.map((item) => {
+              return (
+                <Picker.Item key={item.id} label={item.nome} value={item.id} />
+              );
+            })}
+          </Picker>
+        </View>
+        <View style={styles.inputView}>
+          <Text onPress={() => setViewCalendar(true)} style={styles.inputData}>
+            {data.toLocaleDateString().split("T")[0]}
+          </Text>
+          {viewCalendar && (
+            <DateTimePicker
+              value={data}
+              onChange={handleCalendar}
+              mode="date"
+              testID="dateTimePicker"
+            />
+          )}
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            placeholder="Observações"
+            style={styles.inputObservacao}
+            value={observacao}
+            onChangeText={setObservacao}
+            multiline={true}
+          />
+        </View>
+        <View style={styles.contentButtons}>
+          <Button title="Salvar" onPress={handleSubmit} />
+          <Button title="Continuar" />
+          <Button title="Cancelar" onPress={() => router.back()} />
+        </View>
       </View>
-      <View style={styles.inputView}>
-        <TextInput
-          placeholder="Observações"
-          style={styles.inputObservacao}
-          value={observacao}
-          onChangeText={setObservacao}
-          multiline={true}
-        />
-      </View>
-      <View style={styles.contentButtons}>
-        <Button title="Salvar" />
-        <Button title="Continuar" />
-        <Button title="Cancelar" onPress={() => router.back()} />
-      </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
